a9b70864d07156246027ba8fac877bc6c659df72
areebakhalid3 <khalidareeba3@gmail.com>
2024-10-13 21:49:34 +0500

    message

--- task6CP.cpp
+++ task6CP.cpp
@@ -21,40 +21,41 @@ return 0;
 
 string calculatehotelprices(string mon,int stays)
 {
-float priceapt=0.0,pricest=0.0,discpr=0.0;
+float priceapt=0.0,pricest=0.0,discpr=0.0,discpr1=0.0;
 string type;
 
 if(mon=="july" ||mon=="July" ||mon=="august"||mon=="August"){
     pricest=76*stays;
-    priceapt=77*stays;
-     if(stays>14){
-    discpr=priceapt-(0.10*priceapt);}
-}
+    priceapt=77*stays;}
 
-if(mon=="may" ||mon=="May" ||mon=="october"||mon=="October"){
+else if(mon=="may" ||mon=="May" ||mon=="october"||mon=="October"){
+
     pricest=50*stays;
     priceapt=65*stays;
 
     if(stays>7&&stays<=14){
-       discpr=pricest-(0.05*pricest);
+       pricest=pricest-(0.05*pricest);
     }
     else if(stays>14){
-       discpr=pricest-(0.30*pricest);
-       discpr=priceapt-(0.10*priceapt);
+       pricest=pricest-(0.30*pricest);
     }
 }
 
-else if(mon=="june" ||mon=="June" ||mon=="september"||mon=="September"){
+else if(mon=="june" ||mon=="June" ||mon=="september"||mon=="September"){ 
+
     pricest=75.20*stays;
     priceapt=68.70*stays;
 
     if(stays>14){
-    discpr=pricest-(0.20*pricest);
-    discpr=priceapt-(0.10*priceapt);
+    pricest=pricest-(0.20*pricest);
     }
 }
+
+if(stays>14){
+    priceapt=priceapt-(0.10*priceapt);}
+
 string result="Apartment: " +to_string(priceapt)+"$.\n" + "Studio: " +to_string(pricest)+"$.";
+
+
 return result;
 }
-
-
